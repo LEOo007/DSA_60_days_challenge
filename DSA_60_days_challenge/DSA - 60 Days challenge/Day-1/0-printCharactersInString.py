a1fda2b32ed8daad4c340ab399e742f636444bd3
LEOo007 <110730823+LEOo007@users.noreply.github.com>
2023-02-16 06:51:47 -0800

    Add files via upload

---
+++ DSA_60_days_challenge/DSA - 60 Days challenge/Day-1/0-printCharactersInString.py
@@ -0,0 +1,7 @@
+def printCharactersInString(word):
+  for char in word:
+    print(char)
+  return -1
+
+string = " Hello here i m printing strings in python " 
+printCharactersInString(string)
---
+++ DSA_60_days_challenge/DSA - 60 Days challenge/Day-1/finding chracter in a string.py
@@ -0,0 +1,19 @@
+# Initializing string
+ini_string = 'abcdef'
+ 
+# Character to find
+c = "b"
+# printing initial string and character
+print("initial_string : ", ini_string, "\ncharacter_to_find : ", c)
+ 
+# Using Naive Method
+res = None
+for i in range(0, len(ini_string)):
+    if ini_string[i] == c:
+        res = i + 1
+        break
+ 
+if res == None:
+    print("No such character available in string")
+else:
+    print("Character {} is present at {}".format(c, str(res)))
